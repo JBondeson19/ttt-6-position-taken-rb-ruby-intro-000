f03c4fca59af340bc75885374d21c1978ebda991
James P Bondeson <jpbond58@gmail.com>
2020-07-11 04:50:37 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
 def position_taken? (board, index)
-  !(board[index] == " " ) || (board[index] == "X" || "O")
+  !(board[index] == " " ) && (board[index] == "X" || "O")
 end
 # code your #position_taken? method here!
